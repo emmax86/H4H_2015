2260595643844a1df57fc1cfe431090192bef8e3
Steve King, Jr <stevekx86@gmail.com>
2015-10-03 13:15:12 -0700

    Added registration.

--- Server/src/webapp/views.py
+++ Server/src/webapp/views.py
@@ -1,7 +1,25 @@
-from __init__ import app
+from __init__ import app, db
 from flask import request
+from models import *
 
 
 @app.route("/")
 def hello():
     return "Guard Dog API v0.1.0"
+
+
+@app.route("/register")
+def register():
+    json = request.json
+    name = json["name"]
+    phone = json["phone_number"]
+    password = json["password"]
+    user = User(name, phone, password)
+
+    if User.query.filter_by(phone_number=phone):
+        return "Error, phone number already registered", 401
+
+    db.session.add(user)
+    db.session.commit()
+
+    return "", 200
