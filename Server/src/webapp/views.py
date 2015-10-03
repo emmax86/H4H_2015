759f9ede7f64db0818c0121a65da3597c7608040
Steve King, Jr <stevekx86@gmail.com>
2015-10-03 14:22:52 -0700

    stuff.

--- Server/src/webapp/views.py
+++ Server/src/webapp/views.py
@@ -4,49 +4,58 @@ from flask import abort
 from flask import json
 from models import *
 
-@app.route('/')
-def hello():
-    return 'Guard Dog API v0.1.0'
 
-@app.route('/create')
+@app.route("/")
+def hello():
+    return "Guard Dog API v0.1.0"
+
+
+@app.route("/create")
 def create():
     db.create_all()
-    return 'Great Success', 200
+    return "Great Success", 200
 
-@app.route('/data', methods=['POST'])
+
+@app.route("/data", methods=["POST"])
 def data():
     def verify_structure(json):
         if not request.json:
             return False
-        elif (not 'real' in json) or (not 'frames' in json):
+        elif ("real" not in json) or ("frames" not in json) or ("phone_number" not in json):
             return False
         return True
 
     if not verify_structure(request.json):
-        abort(400)
+        abort(401)
 
-    real = bool(request.json['real'])
+    real = bool(request.json["real"])
 
-    happening = Incident(real)
+    user = User.query.filter_by(request.json["phone_number"]).first()
+
+    if not user:
+        abort(401)
+
+    happening = Incident(real, user)
     db.session.add(happening)
     db.session.commit()
 
-    frames = request.json['frames']
-    for each in sorted(frames, key=lambda frame: frame['batch_order']):
-        frame = AccelerationFrame(each['accel_x'], each['accel_y'], each['accel_z'], happening)
+    frames = request.json["frames"]
+    for each in sorted(frames, key=lambda frame: frame["batch_order"]):
+        frame = AccelerationFrame(each["accel_x"], each["accel_y"], each["accel_z"], happening)
         db.session.add(frame)
 
     db.session.commit()
-    return 'Great Success', 200
+    return "Great Success", 200
 
 
-@app.route('/debug_db')
+@app.route("/debug_db")
 def debug_db():
-        return json.dumps(
-            {
-                'Incidents' : [each.serialize() for each in Incident.query.all()]
-            }
-        ), 200
+    return json.dumps(
+        {
+            "Incidents": [each.serialize() for each in Incident.query.all()]
+        }
+    ), 200
+
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -54,10 +63,12 @@ def register():
     name = json["name"]
     phone = json["phone_number"]
     password = json["password"]
-    user = User(name, phone, password)
+    warranty = json["warranty"]
 
     if User.query.filter_by(phone_number=phone):
         return "Error, phone number already registered", 401
+
+    user = User(name, phone, password, warranty)
 
     db.session.add(user)
     db.session.commit()
