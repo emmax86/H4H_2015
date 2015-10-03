71b193216463a7fd26093149e288dc4a991f8d5a
johncava <john.kevin.cava@gmail.com>
2015-10-03 15:04:15 -0700

    Added Gene Class for AI system in Client Side

---
+++ ClientAI/src/com/john/Gene.java
@@ -0,0 +1,14 @@
+package com.john;
+
+/**
+ * Gene Class for the Genetic Algorithm used for Learning
+ */
+public class Gene {
+
+    float [] gene;
+
+    Gene(int size){
+        gene = new float[size];
+    }
+    
+}
