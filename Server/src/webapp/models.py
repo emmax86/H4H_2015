82dd2f872445d80b0d93f0b535b987d42076ff5f
Steve King, Jr <stevekx86@gmail.com>
2015-10-03 12:21:02 -0700

    Added user model.

--- Server/src/webapp/models.py
+++ Server/src/webapp/models.py
@@ -1,1 +1,18 @@
 from __init__ import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.String(64)
+    phone_number = db.String(15)
+    password_hash = db.String(120)
+
+    def __init__(self, name, phone, password):
+        self.name = name
+        self.phone_number = phone
+        self.password_hash = generate_password_hash(password, "pbkdf2:sha256:10000")
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
