1d513ac36b68099bce145b665ac861135697f85e
johncava <john.kevin.cava@gmail.com>
2015-10-03 16:01:51 -0700

    Added Matrix Multiplication for Neural Network Forward Propagation

--- ClientAI/src/com/john/NeuralNetwork.java
+++ ClientAI/src/com/john/NeuralNetwork.java
@@ -15,4 +15,20 @@ public class NeuralNetwork {
         weight1 = new float[inputLayerDimensions][numData];
         weight2 = new float[1][numData];
     }
+
+    void forwardPropgate(){
+
+    }
+
+    /*
+    Matrix Multiplication for Forward Propagation
+     */
+    float[][] matrixMultiply(float[][] M1,int x1, int x2, float[][] M2, int y1, int y2){
+        float[][] M3 = new float[x1][y2];
+        for(int i = 0; i < x1; i++){
+            for(int j = 0; j < y2; j++){
+                M3[i][j] += M1[i][j]*M2[j][i];
+            }
+        }
+    }
 }
