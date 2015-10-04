ed2c4583ffd9d9cffa4ebcfd233ba21b9e4ce253
Steve King, Jr <stevekx86@gmail.com>
2015-10-03 17:22:28 -0700

    Login.

--- Server/src/webapp/views.py
+++ Server/src/webapp/views.py
@@ -16,21 +16,34 @@ def create():
     return "Great Success", 200
 
 
+@app.route("/login")
+def login():
+    obj = request.get_json()
+    phone_number = obj["phone_number"]
+    password = obj["password"]
+
+    user = User.query.filter_by(phone_number=phone_number).first()
+    if user.verify_password(password):
+        return "Great Success", 200
+    else:
+        return "Great Failure", 401
+
+
 @app.route("/data", methods=["POST"])
 def data():
-    def verify_structure(json):
-        if not request.json:
+    def verify_structure(obj):
+        if not obj:
             return False
-        elif ("real" not in json) or ("frames" not in json) or ("phone_number" not in json):
+        elif ("real" not in obj) or ("frames" not in obj) or ("phone_number" not in obj):
             return False
         return True
 
-    if not verify_structure(request.json):
+    if not verify_structure(request.get_json()):
         abort(401)
 
-    real = bool(request.json["real"])
+    real = bool(request.get_json()["real"])
 
-    user = User.query.filter_by(phone_number=request.json["phone_number"]).first()
+    user = User.query.filter_by(phone_number=request.get_json()["phone_number"]).first()
 
     if not user:
         abort(401)
@@ -39,7 +52,7 @@ def data():
     db.session.add(happening)
     db.session.commit()
 
-    frames = request.json["frames"]
+    frames = request.get_json()["frames"]
     for each in sorted(frames, key=lambda frame: frame["batch_order"]):
         frame = AccelerationFrame(each["batch_order"], each["accel_x"], each["accel_y"], each["accel_z"], happening)
         db.session.add(frame)
@@ -59,11 +72,11 @@ def debug_db():
 
 @app.route("/register", methods=["POST"])
 def register():
-    json = request.json
-    name = json["name"]
-    phone = json["phone_number"]
-    password = json["password"]
-    warranty = json["warranty"]
+    obj = request.get_json()
+    name = obj["name"]
+    phone = obj["phone_number"]
+    password = obj["password"]
+    warranty = obj["warranty"]
 
     if User.query.filter_by(phone_number=phone).first():
         return "Error, phone number already registered", 401
