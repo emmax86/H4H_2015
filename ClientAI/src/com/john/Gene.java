8bc3394ce82396208978cb7668c8c3b7380cc4e2
johncava <john.kevin.cava@gmail.com>
2015-10-03 18:26:59 -0700

    Added Copy Method for Weights to Gene Array

--- ClientAI/src/com/john/Gene.java
+++ ClientAI/src/com/john/Gene.java
@@ -9,6 +9,27 @@ public class Gene {
 
     Gene(int size){
         gene = new float[size];
+        for( int i = 0; i < size; i++){
+            gene[i] = 0.0f;
+        }
+    }
+    
+    /*
+    Return Length of Gene
+     */
+    int getSize(){
+        return gene.length;
     }
 
+    /*
+    Copy Weight Matrix (2-Dimensional) to
+    Gene Data Structure (1-Dimensional Array)
+     */
+    void copyWeightMatrix(float [][] weight, int rows, int cols){
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                gene[(i*cols)+j] = weight[i][j];
+            }
+        }
+    }
 }
