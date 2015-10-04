6b28c62bd261f35adfaebef83ec63340bad9fe62
Steve King, Jr <stevekx86@gmail.com>
2015-10-03 17:05:52 -0700

    ASDFASDFASDo/jfkas;otilknqrt

---
+++ Android/MyApplication/app/src/main/java/com/bramblellc/myapplication/services/ActionConstants.java
@@ -0,0 +1,7 @@
+package com.bramblellc.myapplication.services;
+
+public class ActionConstants {
+
+    public static final String REGISTER_ACTION = "com.bramblellc.myapplication.services.REGISTER";
+
+}
