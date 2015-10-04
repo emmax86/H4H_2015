a35abc444e92fdc9fcdb8a898e9d10d3689850e6
johncava <john.kevin.cava@gmail.com>
2015-10-03 21:01:17 -0700

    Implemented Initialization of Genetic Population for NN training

--- ClientAI/src/com/john/GeneticAlgorithm.java
+++ ClientAI/src/com/john/GeneticAlgorithm.java
@@ -5,17 +5,36 @@ package com.john;
  */
 public class GeneticAlgorithm {
 
-    GeneticAlgorithm(){
+    float dError, tolerance;
+    NeuralNetwork NN;
+
+    GeneticAlgorithm(int numData, float[][] labeledData){
         //Initialization of Neural Network
+        NN = new NeuralNetwork(4,5,1,numData,labeledData);
         //Start Loop
+        while (dError < tolerance) {
             //Forward Propagate Neural Network
+            NN.forwardPropgate();
             //If error < tolerance or doesn't change
-                //Exit
+            //Exit
             //Get Error and Retrieve top 5; clone all
+        }
     }
 
+    /*
+    Initialize Starting Population for Genetic Algorithm to commence Artificial Evolution
+     */
     void initializePopulation(int popSize){
-
+        float[][][] weights = NN.retrieveWeights();
+        float[][] w1, w2;
+        w1 = weights[1].clone();
+        w2 = weights[2].clone();
+        Gene[] genes1 = new Gene[popSize];
+        Gene[] genes2 = new Gene[popSize];
+        for (int i = 0; i < popSize; i++){
+            genes1[i] = new Gene(w1);
+            genes2[i] = new Gene(w2);
+        }
     }
 
     //Retrieve Top 5
