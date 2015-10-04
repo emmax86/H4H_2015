46ee9a11a63fc889ea04f0802a5ecabe8a7d29a8
Nathan Fegard <nfegard@asu.edu>
2015-10-04 06:05:12 -0700

    added boot receiver

---
+++ Android/MyApplication/app/src/main/java/com/bramblellc/myapplication/receivers/BootUpReceiver.java
@@ -0,0 +1,13 @@
+package com.bramblellc.myapplication.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootUpReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //new PingTask().execute(context);
+    }
+}
