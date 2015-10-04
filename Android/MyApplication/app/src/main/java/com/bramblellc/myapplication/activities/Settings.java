ff0bcab590de8682d091d310ee227b4ba1120cba
Nathan Fegard <nfegard@asu.edu>
2015-10-03 23:44:36 -0700

    all the things

--- Android/MyApplication/app/src/main/java/com/bramblellc/myapplication/activities/Settings.java
+++ Android/MyApplication/app/src/main/java/com/bramblellc/myapplication/activities/Settings.java
@@ -3,21 +3,24 @@ package com.bramblellc.myapplication.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.bramblellc.myapplication.R;
 import com.bramblellc.myapplication.layouts.CustomActionbar;
 import com.bramblellc.myapplication.layouts.FullWidthButton;
-import com.bramblellc.myapplication.layouts.FullWidthCheckbox;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Settings extends Activity {
 
     private CustomActionbar settingsCustomActionbar;
     private FullWidthButton myDogsFullWidthButton;
-    private FullWidthCheckbox phoneAidFullWidthCheckbox;
     private FullWidthButton phoneAidInputFullWidthButton;
     private FullWidthButton testFullWidthButton;
 
@@ -29,7 +32,6 @@ public class Settings extends Activity {
 
         settingsCustomActionbar = (CustomActionbar) findViewById(R.id.settings_custom_actionbar);
         myDogsFullWidthButton = (FullWidthButton) findViewById(R.id.my_dogs_full_width_button);
-        phoneAidFullWidthCheckbox = (FullWidthCheckbox) findViewById(R.id.phone_aid_full_width_checkbox);
         phoneAidInputFullWidthButton = (FullWidthButton) findViewById(R.id.phone_aid_input_full_width_button);
         testFullWidthButton = (FullWidthButton) findViewById(R.id.test_full_width_button);
 
@@ -43,13 +45,6 @@ public class Settings extends Activity {
             @Override
             public void onClick(View v) {
                 myDogsPressed(v);
-            }
-        });
-
-        phoneAidFullWidthCheckbox.getFullWidthCheckbox().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //phoneAidPressed(v);
             }
         });
 
@@ -75,16 +70,67 @@ public class Settings extends Activity {
 
     public void myDogsPressed(View view) {
         new MaterialDialog.Builder(this)
-                .title("Add or remove Guard Dogs")
-                .content("Enter the phone number of the Guard Dog you would like to add or remove.")
+                .title("ADD OR REMOVE A GUARD DOG")
+                .positiveText("ADD")
+                .negativeText("REMOVE")
+                .neutralText("CANCEL")
+                .callback(new MaterialDialog.ButtonCallback() {
+                    @Override
+                    public void onPositive(MaterialDialog dialog) {
+                        addDog();
+                    }
+
+                    @Override
+                    public void onNegative(MaterialDialog dialog) {
+                        removeDog();
+                    }
+                })
+                .show();
+    }
+
+    public void addDog() {
+        new MaterialDialog.Builder(this)
+                .title("ADD A GUARD DOG")
+                .content("Enter the phone number of the Guard Dog you would like to add to your kennel.")
                 .positiveText("Add")
-                .negativeText("Remove")
-                .input("", "this needs to be done", new MaterialDialog.InputCallback() {
+                .negativeText("Cancel")
+                .input("", "", new MaterialDialog.InputCallback() {
                     @Override
                     public void onInput(MaterialDialog dialog, CharSequence input) {
-                        System.out.println("should have collected input. swag swag");
+                        SharedPreferences prefs = getSharedPreferences("GuardDog", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = getSharedPreferences("GuardDog", MODE_PRIVATE).edit();
+                        Set<String> dogSet = prefs.getStringSet("dogs", null);
+                        if (dogSet == null) {
+                            dogSet = new HashSet<>();
+                        }
+                        dogSet.add(input.toString());
+                        editor.putStringSet("dogs", dogSet);
+                        editor.commit();
                     }
                 }).show();
     }
 
+    public void removeDog() {
+        new MaterialDialog.Builder(this)
+                .title("REMOVE A GUARD DOG")
+                .content("Enter the phone number of the Guard Dog you would like to remove from your kennel.")
+                .positiveText("remove")
+                .negativeText("Cancel")
+                .input("", "", new MaterialDialog.InputCallback() {
+                    @Override
+                    public void onInput(MaterialDialog dialog, CharSequence input) {
+                        SharedPreferences prefs = getSharedPreferences("GuardDog", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = getSharedPreferences("GuardDog", MODE_PRIVATE).edit();
+                        Set<String> dogSet = prefs.getStringSet("dogs", null);
+                        if (dogSet.contains(input)) {
+                            dogSet.remove(input.toString());
+                            editor.putStringSet("dogs", dogSet);
+                            editor.commit();
+                        } else {
+                            Toast.makeText(Settings.this, "There was no Guard Dog with the inputted phone number in your kennel.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }).show();
+
+    }
 }
