a63700a6964163e1aba3871e036f2ac656044bff
johncava <john.kevin.cava@gmail.com>
2015-10-03 21:36:08 -0700

    Implemented "Mutate Gene Method"

--- ClientAI/src/com/john/Gene.java
+++ ClientAI/src/com/john/Gene.java
@@ -7,6 +7,7 @@ import java.util.Random;
 public class Gene {
 
     float [][] gene;
+    float fitness;
     Random r = new Random();
 
     Gene(float[][] copy){
@@ -26,4 +27,35 @@ public class Gene {
     float[][] returnGene(){
         return gene;
     }
+
+    /*
+    Mutate the Genes (Randomly Change the values inside the Gene)
+     */
+    void mutateGene(){
+        int numberOfMutates = r.nextInt();
+        for (int i = 0; i < numberOfMutates; i++){
+            int x = r.nextInt(gene.length);
+            int y = r.nextInt(gene[0].length);
+            float value = r.nextFloat();
+            gene[x][y] = value;
+        }
+    }
+
+    /*
+    Set Fitness Level
+    IN THIS CASE, LOWER ERROR IS HIGHER FITNESS; THUS FOR EASE LOWER FITNESS IS OF HIGHER PRECEDENCE IN ARTIFICIAL SELECTION
+     */
+    void setFitnessLevel(float fitness){
+        this.fitness = fitness;
+    }
+
+    /*
+    Retrieve Fitness Level
+     */
+
+    float getFitness(){
+        return fitness;
+    }
+
+
 }
--- ClientAI/src/com/john/GeneticAlgorithm.java
+++ ClientAI/src/com/john/GeneticAlgorithm.java
@@ -7,17 +7,22 @@ public class GeneticAlgorithm {
 
     float dError, tolerance;
     NeuralNetwork NN;
+    Gene[] genes1;
+    Gene[] genes2;
 
     GeneticAlgorithm(int numData, float[][] labeledData){
         //Initialization of Neural Network
         NN = new NeuralNetwork(4,5,1,numData,labeledData);
+        //Initialization Genetic Population
+        initializePopulation(15);
         //Start Loop
         while (dError < tolerance) {
             //Forward Propagate Neural Network
             NN.forwardPropgate();
             //If error < tolerance or doesn't change
             //Exit
-            //Get Error and Retrieve top 5; clone all
+            //Get Error and Retrieve top 5; clone all and mutate
+            mutateGenes();
         }
     }
 
@@ -29,8 +34,8 @@ public class GeneticAlgorithm {
         float[][] w1, w2;
         w1 = weights[1].clone();
         w2 = weights[2].clone();
-        Gene[] genes1 = new Gene[popSize];
-        Gene[] genes2 = new Gene[popSize];
+        genes1 = new Gene[popSize];
+        genes2 = new Gene[popSize];
         for (int i = 0; i < popSize; i++){
             genes1[i] = new Gene(w1);
             genes2[i] = new Gene(w2);
@@ -44,11 +49,19 @@ public class GeneticAlgorithm {
 
     //Breed
     void breed(){
-
+        retrieve();
     }
 
-    //Test
-    void testNeuralNetwork(){
+    void mutateGenes(){
+        for (int i = 0; i < genes1.length; i++){
+            genes1[i].mutateGene();
+        }
+        for (int i = 0; i < genes2.length; i++){
+            genes2[i].mutateGene();
+        }
+    }
+    //update Neural Network
+    void updateNeuralNetwork(){
 
     }
 
